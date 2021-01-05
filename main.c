6b5716cf10059345c9474f526c12d310c7c29ba8
rhino-tower <yukis.a0516@icloud.com>
2021-01-05 21:58:52 +0900

    main func

--- main.c
+++ main.c
@@ -1,19 +1,29 @@
 #include "get_next_line.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
 	char *line;
 	int fd;
 	int g;
 
-	fd = open("test1.txt", O_RDONLY);
-	while ((g = get_next_line(fd, &line)) > 0)
+	if (argc <= 1 || MAX_FD < argc)
+		printf("command line is range(1, 256)\n");
+	else
 	{
-		printf("%d %s\n",g, line);
-		free(line);
+		for (int i = 0; i < argc - 1; i++)
+		{
+			puts("---------------------");
+			printf("file name : %s\n", argv[i + 1]);
+			fd = open(argv[i + 1], O_RDONLY);
+			while ((g = get_next_line(fd, &line)) > 0)
+			{
+				printf("%d %s\n",g, line);
+				free(line);
+			}
+			printf("%d %s\n",g, line);
+			free(line);
+			close(fd);
+		}
 	}
-	printf("%d %s\n",g, line);
-	free(line);
-	close(fd);
 	system("leaks a.out");
 }
